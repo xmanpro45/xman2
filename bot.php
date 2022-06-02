01b2ec2256c55c454700013a4208d5e4f3a56bac
xmanpro45 <106592277+xmanpro45@users.noreply.github.com>
2022-06-02 02:55:14 -0400

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,18 +13,22 @@
     
     $gate1 = "xman2/api.php"; /// Your Checker URl with api.php or chk.php
   $gate2 = "xman2/gen.php";
+  $gate3 = "xman2/rand.php";
+  $gate4 = "xman2/bin.php";
+  $gate5 = "xman2/sk.php";
 if($message == "/start"){
-    send_message($chat_id,$message_id, "Hey $firstname \nUse /cmds to View Commands \n$start_msg");
+    send_message($chat_id,$message_id, "Hey $firstname \nUse /cms to View Commands \n$start_msg");
 }
 
-if($message == "/cmds"){
+if($message == "/cms"){
     send_message($chat_id,$message_id, "
-    !ch xxxxxxxxxxxxxxxx|xx|xxxx|xxx   
+   . ch stripe 1$ |.cm Stripe+woo Charge.
+  . gen cc gen|.rand random adrs |.sk sk chk🖐️
     ");
 }
 
 //Gate 1
-if(strpos($message, "!ch") === 0){
+if(strpos($message, ".st") === 0){
     $cc = substr($message, 4);
     $curl = curl_init();
     curl_setopt_array($curl, [
@@ -51,21 +55,100 @@ if(strpos($message, "!ch") === 0){
 
 }
 
-
-if(strpos($message, "!cm") === 0){
+if(strpos($message, ".gen") === 0){
     $cc = substr($message, 4);
     $curl = curl_init();
     curl_setopt_array($curl, [
-    CURLOPT_URL => "$gate2?lista=$cc",
+    CURLOPT_URL => "$gate2",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+   ],
+   ]);
+
+ $result = curl_exec($curl);
+ curl_close($curl);
+   send_message($chat_id,$message_id, "$result \nChecked By @$username ");
+
+}
+
+if(strpos($message, ".rand") === 0){
+    $cc = substr($message, 4);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "$gate3",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
     CURLOPT_HTTPHEADER => [
     "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+   ],
+   ]);
+
+ $result = curl_exec($curl);
+ curl_close($curl);
+   send_message($chat_id,$message_id, "$result \nChecked By @$username ");
+
+}
+
+if(strpos($message, ".bin") === 0){
+    $cc = substr($message, 4);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "$gate4",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+    "sec-fetch-dest: document",
+    "sec-fetch-site: none",
+    "user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+   ],
+   ]);
+
+ $result = curl_exec($curl);
+ curl_close($curl);
+   send_message($chat_id,$message_id, "$result \nChecked By @$username ");
+
+}
+if(strpos($message, ".bin") === 0){
+    $cc = substr($message, 4);
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+    CURLOPT_URL => "$gate5",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_HTTPHEADER => [
+    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
     "sec-fetch-dest: document",
     "sec-fetch-site: none",
